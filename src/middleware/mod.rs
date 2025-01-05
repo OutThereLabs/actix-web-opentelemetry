27d28b9361c1787034c9dc56bf6cde012e3885cc
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-01-05 14:15:23 -0500

    Update opentelemetry versions to 0.27

--- src/middleware/mod.rs
+++ src/middleware/mod.rs
@@ -1,5 +1,14 @@
+use opentelemetry::InstrumentationScope;
+
 #[cfg(feature = "metrics")]
 #[cfg_attr(docsrs, doc(cfg(feature = "metrics")))]
 pub(crate) mod metrics;
 pub(crate) mod route_formatter;
 pub(crate) mod trace;
+
+pub(crate) fn get_scope() -> InstrumentationScope {
+    InstrumentationScope::builder("actix-web-opentelemetry")
+        .with_version(env!("CARGO_PKG_VERSION"))
+        .with_schema_url(opentelemetry_semantic_conventions::SCHEMA_URL)
+        .build()
+}
